2c5bde40c62a7c0903eda7ea3375dd32642a2c19
sborkowski <sborkowski@microstrategy.com>
2022-03-24 16:46:27 +0100

    Connected to blockchain and smart contracts

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,5 +2,22 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList {
-    uint256 public TaskNumber = 0;
+    uint256 public taskNumber = 0;
+
+    struct Task {
+        uint256 id;
+        string description;
+        bool isCompleted;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    function createTask(string memory _content) public {
+        taskNumber++;
+        tasks[taskNumber] = Task(taskNumber, _content, false);
+    }
+
+    constructor() public {
+        createTask("first task");
+    }
 }
---
+++ src/app.js
@@ -0,0 +1,61 @@
+App = {
+  web3Provider: null,
+  account: null,
+  contracts: {},
+
+  init: async function() {
+    console.log('app loading ...')
+    return App.initWeb3();
+  },
+
+  initWeb3: async function() {
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      App.web3Provider = window.ethereum;
+      try {
+        // Request account access
+        await window.ethereum.request({ method: "eth_requestAccounts" });;
+      } catch (error) {
+        // User denied account access...
+        console.error("User denied account access")
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      App.web3Provider = window.web3.currentProvider;
+    }
+    // If no injected web3 instance is detected, fall back to Ganache
+    else {
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+    
+    return App.loadAccount();
+  },
+
+  loadAccount: async () => {
+    // Set the current blockchain account
+    const web3 = new Web3(App.web3Provider);
+    App.account = web3.eth.accounts[0]
+    return App.initContract()
+  },
+
+
+  initContract: async () => {
+    // Create a JavaScript version of the smart contract
+    const todoList = await $.getJSON('TodoList.json')
+    App.contracts.TodoList = TruffleContract(todoList)
+    App.contracts.TodoList.setProvider(App.web3Provider)
+
+    // Hydrate the smart contract with values from the blockchain
+    App.todoList = await App.contracts.TodoList.deployed()
+  },
+
+
+
+};
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  });
+});
